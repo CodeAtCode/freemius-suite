dbedc479418059b0d0e80748240078b0e131302d
Daniele Scasciafratte <mte90net@gmail.com>
2016-12-16 13:06:38 +0100

    package a plugin

---
+++ package.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+pluginfolder=$1
+originalfoldername=`basename $pluginfolder`
+wd=$PWD
+if [ -z $1 ]; then
+    originalfoldername=`basename $PWD`
+fi
+fileroot=$2
+if [ -z $2 ]; then
+    fileroot=`basename $pluginfolder`
+fi
+
+r=$(( $RANDOM % 10 ));
+foldername="$originalfoldername-$r"
+
+echo "Generating the zip in progress..."
+
+cp -ar $pluginfolder /tmp/$foldername
+
+cd /tmp/$foldername
+
+version=`grep "^Stable tag:" README.txt | awk -F' ' '{print $NF}'`
+
+echo "Cleaning in Progress..."
+rm -rf ./.git*
+rm -rf ./.sass-cache
+rm -rf ./.directory
+rm -rf ./node_modules
+rm -rf ./wp-config-test.php
+rm -rf ./*.yml
+rm -rf ./package.json
+rm -rf ./Gruntfile.js
+rm -rf ./.netbeans*
+rm -rf ./.php_cs
+rm -rf ./assets
+rm -rf ./admin/assets/sass
+rm -rf ./admin/assets/coffee
+rm -rf ./public/assets/sass
+rm -rf ./public/assets/coffee
+rm -rf ./*.zip
+#This contain the test stuff
+rm -rf ./vendor
+rm -rf ./tests
+
+echo "Downloading clean composer dependencies..."
+composer update --no-dev &> /dev/null
+
+#Remove Fake_Freemius
+if [ -f './includes/Fake_Freemius.php' ]; then
+    echo "Cleaning for Freemius"
+    rm -rf ./includes/Fake_Freemius.php
+    rowff=`grep -n "/includes/Fake_Freemius.php" $fileroot.php | awk -F: '{print $1}'`
+    rowff+='d'
+    sed -i "$rowff" $fileroot.php
+    #If Freemius SDK is commented remove the comments
+    rowfs=`grep -n "/includes/freemius/start.php" $fileroot.php | awk -F: '{print $1}'`
+    rowfs+='s'
+    sed -i "$rowfs/\/\///" $fileroot.php
+fi
+
+zip -r $wd/$originalfoldername-$version.zip ./ &> /dev/null
+
+rm -rf /tmp/$foldername
+
+echo "Done!"
