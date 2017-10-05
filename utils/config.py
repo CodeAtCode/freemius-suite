eb02be986d4e0aec7e0b2462885a2ad43d5e1c5d
Daniele Scasciafratte <mte90net@gmail.com>
2017-10-05 13:23:44 +0200

    get config file path

--- utils/config.py
+++ utils/config.py
@@ -1,14 +1,16 @@
 import sys, configparser, os.path
 
+path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+path = path + 'config.ini'
 
 if len(sys.argv) == 0:
     print('The folder of the script is required!')
     sys.exit()
 
-if os.path.isfile('config.ini'):
+if os.path.isfile(path):
     # Load configuration
     ini = configparser.RawConfigParser()
-    ini.readfp(open('config.ini'))
+    ini.readfp(open(path))
 else:
     print('Configuration file is missing!')
     sys.exit()
