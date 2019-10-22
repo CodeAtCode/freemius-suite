7aa024200889833a341209a54032682b65c4e11c
Daniele Scasciafratte <mte90net@gmail.com>
2019-10-22 12:47:54 +0200

    not generate again the package if exist

--- package.sh
+++ package.sh
@@ -34,79 +34,84 @@ cd "$pluginfolder" || exit
 
 version=$(grep "^Stable tag:" README.txt | awk -F' ' '{print $NF}')
 
-git tag -a "$version" -m "$version"
-git checkout master
-git push origin "$version"
-echo "-Created the git tag for $version version"
+if [ -f "$output"/"$packagename"-"$version".zip ]; then
+    echo "-Package already exists, ignore the package generation"
+    slack-message "Package uploading for $packagename at $version in progress..."
+else
+    git tag -a "$version" -m "$version"
+    git checkout master
+    git push origin "$version"
+    echo "-Created the git tag for $version version"
 
-cp -r "$pluginfolder" /tmp/"$foldername"
-cd /tmp/"$foldername" || exit
+    cp -r "$pluginfolder" /tmp/"$foldername"
+    cd /tmp/"$foldername" || exit
 
-echo "-Generating the zip in progress..."
+    echo "-Generating the zip in progress..."
 
-echo "-Cleaning in Progress..."
-rm -rf ./.git*
-rm -rf ./.sass-cache
-rm -rf ./.directory
-rm -rf ./node_modules
-rm -rf ./nbproject
-rm -rf ./composer/
-rm -rf ./wp-config-test.php
-rm -rf ./*.yml
-rm -rf ./*.neon
-rm -rf ./*.ini
-rm -rf ./*.sh
-rm -rf ./.*.cache
-rm -rf ./.gitignore
-rm -rf ./.eslintrc.json
-rm -rf ./psalm.xml
-rm -rf ./codeat.xml
-rm -rf ./package.json
-rm -rf ./package-lock.json
-rm -rf ./Gruntfile.js
-rm -rf ./gulpfile.js
-rm -rf ./composer.lock
-rm -rf ./.netbeans*
-rm -rf ./.travis*
-rm -rf ./.php_cs
-rm -rf ./.padawan*
-rm -rf ./assets
-rm -rf ./admin/assets/sass
-rm -rf ./admin/assets/coffee
-rm -rf ./public/assets/sass
-rm -rf ./public/assets/coffee
-rm -rf ./*.zip
-#This contain the test stuff
-rm -rf ./vendor
-rm -rf ./tests
+    echo "-Cleaning in Progress..."
+    rm -rf ./.git*
+    rm -rf ./.sass-cache
+    rm -rf ./.directory
+    rm -rf ./node_modules
+    rm -rf ./nbproject
+    rm -rf ./composer/
+    rm -rf ./wp-config-test.php
+    rm -rf ./*.yml
+    rm -rf ./*.neon
+    rm -rf ./*.ini
+    rm -rf ./*.sh
+    rm -rf ./.*.cache
+    rm -rf ./.gitignore
+    rm -rf ./.eslintrc.json
+    rm -rf ./psalm.xml
+    rm -rf ./codeat.xml
+    rm -rf ./package.json
+    rm -rf ./package-lock.json
+    rm -rf ./Gruntfile.js
+    rm -rf ./gulpfile.js
+    rm -rf ./composer.lock
+    rm -rf ./.netbeans*
+    rm -rf ./.travis*
+    rm -rf ./.php_cs
+    rm -rf ./.padawan*
+    rm -rf ./assets
+    rm -rf ./admin/assets/sass
+    rm -rf ./admin/assets/coffee
+    rm -rf ./public/assets/sass
+    rm -rf ./public/assets/coffee
+    rm -rf ./*.zip
+    #This contain the test stuff
+    rm -rf ./vendor
+    rm -rf ./tests
 
-if [ -s './composer.json' ]; then
-    #Detect if there are composer dependencies
-    dep=$(cat "./composer.json" | jq 'has(".require")')
-    if [ "$dep" == 'true' ]; then
-        echo "-Downloading clean composer dependencies..."
-        composer update --no-dev &> /dev/null
-    else
-        rm -rf ./composer.json
+    if [ -s './composer.json' ]; then
+        #Detect if there are composer dependencies
+        dep=$(cat "./composer.json" | jq 'has(".require")')
+        if [ "$dep" == 'true' ]; then
+            echo "-Downloading clean composer dependencies..."
+            composer update --no-dev &> /dev/null
+        else
+            rm -rf ./composer.json
+        fi
     fi
+
+    #Remove Fake_Freemius - it is the only requirement for Freemius
+    if [ -s './includes/Fake_Freemius.php' ]; then
+        echo "-Cleaning for Freemius"
+        rm -rf ./includes/Fake_Freemius.php
+        rowff=$(grep -n "/includes/Fake_Freemius.php" "$fileroot" | awk -F: '{print $1}')
+        rowff+='d'
+        sed -i "$rowff" "$fileroot"
+        #If Freemius SDK is commented remove the comments
+        rowfs=$(grep -n "/includes/freemius/start.php" "$fileroot" | awk -F: '{print $1}')
+        rowfs+='s'
+        sed -i "$rowfs/\\/\\///" "$fileroot"
+    fi
+
+    zip -r "$output"/"$packagename"-"$version".zip ./ &> /dev/null
+
+    slack-message "Package generated for $packagename at $version done!"
+    rm -rf /tmp/"$foldername"
+
+    echo " -Package generated!"
 fi
-
-#Remove Fake_Freemius - it is the only requirement for Freemius
-if [ -s './includes/Fake_Freemius.php' ]; then
-    echo "-Cleaning for Freemius"
-    rm -rf ./includes/Fake_Freemius.php
-    rowff=$(grep -n "/includes/Fake_Freemius.php" "$fileroot" | awk -F: '{print $1}')
-    rowff+='d'
-    sed -i "$rowff" "$fileroot"
-    #If Freemius SDK is commented remove the comments
-    rowfs=$(grep -n "/includes/freemius/start.php" "$fileroot" | awk -F: '{print $1}')
-    rowfs+='s'
-    sed -i "$rowfs/\\/\\///" "$fileroot"
-fi
-
-zip -r "$output"/"$packagename"-"$version".zip ./ &> /dev/null
-
-slack-message "Package generated for $packagename at $version done!"
-rm -rf /tmp/"$foldername"
-
-echo " -Package generated!"
